1bcaad882882fa98f7a2b57e0b3a938b585c8272
PriyankaJayanthi <98743923+PriyankaJayanthi@users.noreply.github.com>
2022-06-06 22:19:35 +0200

    First commit

---
+++ MVC_ViewModels_Data/Data/ExDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_ViewModels_Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_ViewModels_Data.Data
+{
+    public class ExDbContext : DbContext
+    {
+        public ExDbContext(DbContextOptions<ExDbContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {             
+            //countries
+            Country England = new Country() { CountryId = 1, Name = "England" };
+            Country Sweden = new Country() { CountryId = 2, Name = "Sweden" };
+            modelBuilder.Entity<Country>().HasData(England, Sweden);
+
+            //cities
+            City London = new City() {  CityId = 1, Name = "London", CountryId = England.CountryId };
+            City Birmingham = new City() { CityId = 2, Name = "Birmingham", CountryId = England.CountryId };
+            City Stockholm = new City() { CityId = 3, Name = "Stockholm", CountryId = Sweden.CountryId };
+            City Gothenburg = new City() { CityId = 4, Name = "Gothenburg", CountryId = Sweden.CountryId };
+            modelBuilder.Entity<City>().HasData(London, Birmingham, Stockholm, Gothenburg);
+
+            // Persons
+            Person priya = new Person() { PersonId = 1, Name = "Priya", ContactNumber = "034-4242-657", CityId = London.CityId };
+            Person Keerthi = new Person() { PersonId = 2, Name = "Keerthi", ContactNumber = "034-4242-658", CityId = Gothenburg.CityId };
+            modelBuilder.Entity<Person>().HasData(priya, Keerthi);
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>()
+                .HasOne(e => e.Country)
+                .WithMany(e => e.Cities);
+
+            modelBuilder.Entity<Person>()
+                .HasOne(e => e.City)
+                .WithMany(e => e.Peoples);
+
+            modelBuilder.Entity<Country>()
+                .HasMany(e => e.Cities)
+                .WithOne(e => e.Country);
+        }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<City> cities { get; set; }
+        public DbSet<Country> countries { get; set; }
+    }
+}
---
+++ MVC_ViewModels_Data/Migrations/20220606172420_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_ViewModels_Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "countries",
+                columns: table => new
+                {
+                    CountryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_countries", x => x.CountryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cities",
+                columns: table => new
+                {
+                    CityId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cities", x => x.CityId);
+                    table.ForeignKey(
+                        name: "FK_cities_countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "countries",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    ContactNumber = table.Column<string>(nullable: false),
+                    CityId = table.Column<int>(maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_Person_cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "cities",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "countries",
+                columns: new[] { "CountryId", "Name" },
+                values: new object[] { 1, "England" });
+
+            migrationBuilder.InsertData(
+                table: "countries",
+                columns: new[] { "CountryId", "Name" },
+                values: new object[] { 2, "Sweden" });
+
+            migrationBuilder.InsertData(
+                table: "cities",
+                columns: new[] { "CityId", "CountryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "London" },
+                    { 2, 1, "Birmingham" },
+                    { 3, 2, "Stockholm" },
+                    { 4, 2, "Gothenburg" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Person",
+                columns: new[] { "PersonId", "CityId", "ContactNumber", "Name" },
+                values: new object[] { 1, 1, "034-4242-657", "Priya" });
+
+            migrationBuilder.InsertData(
+                table: "Person",
+                columns: new[] { "PersonId", "CityId", "ContactNumber", "Name" },
+                values: new object[] { 2, 4, "034-4242-658", "Keerthi" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cities_CountryId",
+                table: "cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_CityId",
+                table: "Person",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "cities");
+
+            migrationBuilder.DropTable(
+                name: "countries");
+        }
+    }
+}
